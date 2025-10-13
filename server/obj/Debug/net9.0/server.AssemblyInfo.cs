d0de7f13a7769a461fc930d7b1617fa85e397558
JonLlo <jonnylloydie2@gmail.com>
2025-10-13 18:56:48 +0100

    7pm

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d0910caa2df7b6532fcd04c8e18ccdc79908075")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8a5e62125edc916d447f68c7ef1e39b7eebfab")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
